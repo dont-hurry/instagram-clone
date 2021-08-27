de0e8b90701a945326f310a81021960846c512a1
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2021-08-27 09:53:00 +0800

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import * as ROUTES from "./constants/routes";
+import LandingPage from "./pages/landing";
+import LogInPage from "./pages/log-in";
+import SignUpPage from "./pages/sign-up";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <LandingPage />
+        </Route>
+        <Route path={ROUTES.LOG_IN}>
+          <LogInPage />
+        </Route>
+        <Route path={ROUTES.SIGN_UP}>
+          <SignUpPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
---
+++ src/constants/routes.js
@@ -0,0 +1,2 @@
+export const LOG_IN = "/accounts/login/";
+export const SIGN_UP = "/accounts/signup/";
