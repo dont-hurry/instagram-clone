4ec3271f1bd26a4b793316e89dec41ae1ab886f9
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2021-08-29 12:30:54 +0800

    Update UI of comments

--- src/components/Post/Comments.js
+++ src/components/Post/Comments.js
@@ -1,3 +1,20 @@
-export default function Comments() {
-  return <div>Comments</div>;
+import styles from "./Comments.module.css";
+
+function Comment({ comment: { username, comment } }) {
+  return (
+    <div>
+      <span className={styles.username}>{username}</span>{" "}
+      <span className={styles.comment}>{comment}</span>
+    </div>
+  );
+}
+
+export default function Comments({ comments }) {
+  return (
+    <div className={styles.container}>
+      {comments.map((comment, index) => (
+        <Comment key={index} comment={comment} />
+      ))}
+    </div>
+  );
 }
