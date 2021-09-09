56b9a38387cc24ff01f0c8493972e97810ee798b
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2021-09-10 02:28:20 +0800

    Add loading state in profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,25 +2,34 @@ import { useParams } from "react-router";
 import { useState, useEffect } from "react";
 import { getUserInfoByUsername } from "../services/firebase";
 import NavigationLayout from "../components/layout/navigation";
+import Loading from "../components/icons/Loading";
 import NotFound from "../components/NotFound";
 
 export default function Profile() {
   const { username } = useParams();
   const [userInfo, setUserInfo] = useState(null);
+  const [isLoadingUserInfo, setIsLoadingUserInfo] = useState(true);
 
   useEffect(() => {
     (async () => {
       const returnedUserInfo = await getUserInfoByUsername(username);
       setUserInfo(returnedUserInfo);
+      setIsLoadingUserInfo(false);
     })();
   }, [username]);
 
   useEffect(() => {
+    if (isLoadingUserInfo) return;
+
     const title = userInfo
       ? `${userInfo.fullName} (@${userInfo.username})`
       : "Page Not Found";
     document.title = `${title} - Instagram`;
-  }, [userInfo]);
+  }, [isLoadingUserInfo, userInfo]);
+
+  if (isLoadingUserInfo) {
+    return <Loading />;
+  }
 
   if (!userInfo) {
     return (
