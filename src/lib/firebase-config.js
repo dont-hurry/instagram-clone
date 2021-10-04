29267d4a178f6a455d48276db437911cf4ca9f8a
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2021-10-04 16:11:22 +0800

    Ready for deploy

---
+++ src/lib/firebase-config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyB-pPHMpBVieUrrTt3shIio6pDznZC_H5c",
+  authDomain: "instagram-clone-21523.firebaseapp.com",
+  projectId: "instagram-clone-21523",
+  storageBucket: "instagram-clone-21523.appspot.com",
+  messagingSenderId: "305666557686",
+  appId: "1:305666557686:web:63c6af131613e5950b0ed7",
+};
+
+export default config;
